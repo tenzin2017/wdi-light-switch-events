86724b56971199ef5720d18aeb752ac3107bf14d
tenzin2017 <tenzinleo@gmail.com>
2017-10-02 15:01:21 -0400

    event handler switch , used if-else condition to switch on and off

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
+var h1 = document.querySelector('h1');
+var btn = document.querySelector('button');
+var body = document.querySelector('body');
+btn.addEventListener('click', function() {
+  if (btn.className === 'switch on') {
+    body.className = 'dark';
+    btn.className = 'switch off';
+    h1.innerText = 'Hey, who turned off the light ?'
+  } else {
+    btn.className = 'switch on';
+    body.className = 'light';
+    h1.innerText = "It's so bright in here !"
+  }
 
-
+});
